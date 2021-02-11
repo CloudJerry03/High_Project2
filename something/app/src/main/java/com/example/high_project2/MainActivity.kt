8ad02699298c77e2b9069ed9aab9a64e0facfd84
yubinkim <kimu2371@naver.com>
2021-02-11 10:25:57 +0900

    dataBinding

--- something/app/src/main/java/com/example/high_project2/MainActivity.kt
+++ something/app/src/main/java/com/example/high_project2/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.high_project2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.high_project2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
