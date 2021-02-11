1a16614dfc0bf8b0cb4ea1b126c61cd60257bdd3
yubinkim <kimu2371@naver.com>
2021-02-11 10:59:58 +0900

    android->js (fun android_to_js)

--- something/app/src/main/java/com/example/high_project2/MainActivity.kt
+++ something/app/src/main/java/com/example/high_project2/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.high_project2
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -18,13 +20,26 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.webView.settings.javaScriptEnabled=true
+        binding.webView.setWebChromeClient(object: WebChromeClient(){})
         binding.webView.setWebViewClient(object: WebViewClient(){})
+        TODO("url 작성")
+        binding.webView.loadUrl("file:///android_asset/index.html")
 
         //asset db파일 불러오기
         val adb=AssetDatabaseOpenHelper(this)
         adb.openDatabase()
 
 
+        //원하는 데이터 전달
 
+
+
+
+    }
+    //안드로이드->웹뷰 데이터 전달
+    @JavascriptInterface
+    fun android_to_js(msg:String){
+        TODO("loadUrl작성성")
+        binding.webView.loadUrl("javascript:~~~~('"+msg+"')")
     }
 }
